b167d4ba51665e6c1975f4fdd2acbaf65a3f7804
showanne <showanne.e@gmail.com>
2021-05-10 12:14:21 +0800

    嘗試判斷股票代號

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import axios from 'axios'
-// import iconv from 'iconv-lite'
+import iconv from 'iconv-lite'
 
 // 讓套件讀取 .env 檔案
 dotenv.config()
@@ -12,6 +12,32 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
+const arrSymbolId = []
+const getlist = async () => {
+  try {
+    const { data } = await axios.get('https://members.sitca.org.tw/OPF/K0000/files/F/01/stock.txt', {
+      responseType: 'arraybuffer',
+      transformResponse: [
+        data => {
+          // 將檔案編碼改big5
+          return iconv.decode(Buffer.from(data), 'big5')
+        }
+      ]
+    })
+    // 文字轉成陣列 .split(分割文字)
+    const arr = data.split('\r\n')
+    for (const i in arr) {
+      arr[i] = arr[i].split(',')
+      arrSymbolId.push(arr[i][0])
+    }
+
+    // console.log(arrSymbolId)
+  } catch (error) {
+    console.log(error)
+  }
+}
+getlist()
+
 bot.listen('/', process.env.PORT, () => {
   console.log('機器人啟動')
 })
@@ -19,44 +45,48 @@ bot.listen('/', process.env.PORT, () => {
 // async <-> await
 bot.on('message', async event => {
   console.log(event)
-  if (event.message.type === 'text') {
-    try {
-      const responseChart = await axios.get(
-        `https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
-      )
-      const responseMeta = await axios.get(
-        `https://api.fugle.tw/realtime/v0/intraday/meta?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
-      )
-      const responseQuote = await axios.get(
-        `https://api.fugle.tw/realtime/v0/intraday/quote?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
-      )
-      const responseDealts = await axios.get(
-        `https://api.fugle.tw/realtime/v0.2/intraday/dealts?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0&limit=5`
-      )
-      console.log(responseChart.data.data.chart)
-      // console.log(responseQuote.data.data.quote.change)
-      const reply = `股票中文簡稱：${responseMeta.data.data.meta.nameZhTw}
-        \n股票代號：${responseMeta.data.data.info.symbolId}
-        \n此分鐘的：${responseChart.data.data.chart}
-        \n此分鐘的開盤價：${responseChart.data.data.chart.open}
-        \n此分鐘的最高價：${responseChart.data.data.chart.high}
-        \n此分鐘的最低價：${responseChart.data.data.chart.low}
-        \n此分鐘的收盤價：${responseChart.data.data.chart.close}
-        \n此分鐘的交易張數：${responseChart.data.data.chart.unit}
+  if (getlist.includes(event.message.type)) {
+    if (event.message.type === 'text') {
+      try {
+        const responseChart = await axios.get(
+          `https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
+        )
+        const responseMeta = await axios.get(
+          `https://api.fugle.tw/realtime/v0/intraday/meta?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
+        )
+        const responseQuote = await axios.get(
+          `https://api.fugle.tw/realtime/v0/intraday/quote?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
+        )
+        const responseDealts = await axios.get(
+          `https://api.fugle.tw/realtime/v0.2/intraday/dealts?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0&limit=5`
+        )
+        console.log(responseChart.data.data.chart)
+        // console.log(responseQuote.data.data.quote.change)
+        const reply = `股票中文簡稱：${responseMeta.data.data.meta.nameZhTw}
+          \n股票代號：${responseMeta.data.data.info.symbolId}
+          \n此分鐘的：${responseChart.data.data.chart}
+          \n此分鐘的開盤價：${responseChart.data.data.chart.open}
+          \n此分鐘的最高價：${responseChart.data.data.chart.high}
+          \n此分鐘的最低價：${responseChart.data.data.chart.low}
+          \n此分鐘的收盤價：${responseChart.data.data.chart.close}
+          \n此分鐘的交易張數：${responseChart.data.data.chart.unit}
+  
+          \n最新一筆成交時間：${new Date(responseQuote.data.data.quote.total.at).toLocaleString('zh-tw')}
+          \n今日參考價：${responseMeta.data.data.meta.priceReference}
+          \n漲停價：${responseMeta.data.data.meta.priceHighLimit}
+          \n跌停價：${responseMeta.data.data.meta.priceLowLimit}
+          \n當日股價之漲跌：${parseInt(responseQuote.data.data.quote.change)}
+          \n跌停價：${responseDealts.data.data.dealts}
+          `
 
-        \n最新一筆成交時間：${new Date(responseQuote.data.data.quote.total.at).toLocaleString('zh-tw')}
-        \n今日參考價：${responseMeta.data.data.meta.priceReference}
-        \n漲停價：${responseMeta.data.data.meta.priceHighLimit}
-        \n跌停價：${responseMeta.data.data.meta.priceLowLimit}
-        \n當日股價之漲跌：${parseInt(responseQuote.data.data.quote.change)}
-        \n跌停價：${responseDealts.data.data.dealts}
-        `
-
-      event.reply(reply)
-    } catch (error) {
-      console.log(error)
-      // event.reply(error)
-      event.reply('發生錯誤QQ')
+        event.reply(reply)
+      } catch (error) {
+        console.log(error)
+        // event.reply(error)
+        event.reply('發生錯誤QQ')
+      }
     }
+  } else {
+    event.reply('沒有這個股票喔OwO')
   }
 })
