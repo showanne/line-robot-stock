ec56e97364384f595d8a18183d72ec2a8fba4ccf
showanne <showanne.e@gmail.com>
2021-05-10 23:44:40 +0800

    嘗試  FLEX MESSAGE 資料呈現 OK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import axios from 'axios'
+import fs from 'fs'
 import iconv from 'iconv-lite'
 
 // 讓套件讀取 .env 檔案
@@ -44,8 +45,8 @@ bot.listen('/', process.env.PORT, () => {
 // async <-> await
 bot.on('message', async event => {
   console.log(event)
+
   if (event.message.type === 'text') {
-    // if (getlist.includes(event.message.text)) {
     try {
       const responseChart = await axios.get(
         `https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
@@ -56,58 +57,318 @@ bot.on('message', async event => {
       const responseQuote = await axios.get(
         `https://api.fugle.tw/realtime/v0.2/intraday/quote?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0`
       )
-      const responseDealts = await axios.get(
-        `https://api.fugle.tw/realtime/v0.2/intraday/dealts?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0&limit=9`
-      )
-      const googleFin = await axios.get(`https://www.google.com/finance/quote/${encodeURI(event.message.text)}:TPE`)
-
-      // console.log(responseChart.data.data.chart)
-      // console.log(responseQuote.data.data.quote.change)
+      // const responseDealts = await axios.get(
+      //   `https://api.fugle.tw/realtime/v0.2/intraday/dealts?symbolId=${encodeURI(event.message.text)}&apiToken=bcb3f1d25b0a8e5d3ad0e7acbdbe10b0&limit=9`
+      // )
+      // const googleFin = await axios.get(`https://www.google.com/finance/quote/${encodeURI(event.message.text)}:TPE`)
 
       for (const c in responseChart.data.data.chart) {
-        console.log(c.substr(11, 5))
-        var chartMin = c.substr(11, 5)
+        // console.log(c.substr(11, 5))
+        // var chartMin = c.substr(11, 5)
         var chartData = responseChart.data.data.chart[c]
-        console.log(chartMin)
-        console.log(chartData)
+        // console.log(chartMin)
+        // console.log(chartData)
       }
 
-      for (const d in responseDealts.data.data.dealts) {
-        var dealtsData = responseDealts.data.data.dealts[d]
-        console.log(dealtsData)
+      // for (const d in responseDealts.data.data.dealts) {
+      // var dealtsData = responseDealts.data.data.dealts[d]
+      // console.log(dealtsData)
+      // }
+
+      const flex = {
+        type: 'bubble',
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: 'Detail',
+              weight: 'bold',
+              color: '#1DB446',
+              size: 'sm'
+            },
+            {
+              type: 'text',
+              text: `${responseMeta.data.data.info.symbolId}`,
+              weight: 'bold',
+              size: 'xxl',
+              margin: 'md'
+            },
+            {
+              type: 'text',
+              text: `${responseMeta.data.data.meta.nameZhTw}`,
+              size: 'xs',
+              color: '#aaaaaa',
+              wrap: true
+            },
+            {
+              type: 'separator',
+              margin: 'xxl'
+            },
+            {
+              type: 'box',
+              layout: 'vertical',
+              margin: 'xxl',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '今日參考價',
+                      size: 'sm',
+                      color: '#555555',
+                      flex: 0
+                    },
+                    {
+                      type: 'text',
+                      text: `${responseMeta.data.data.meta.priceReference}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '漲停價',
+                      size: 'sm',
+                      color: '#555555',
+                      flex: 0
+                    },
+                    {
+                      type: 'text',
+                      text: `${responseMeta.data.data.meta.priceHighLimit}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '跌停價',
+                      size: 'sm',
+                      color: '#555555',
+                      flex: 0
+                    },
+                    {
+                      type: 'text',
+                      text: `${responseMeta.data.data.meta.priceLowLimit}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '今日漲跌幅',
+                      size: 'sm',
+                      color: '#555555',
+                      flex: 0
+                    },
+                    {
+                      type: 'text',
+                      text: `${responseQuote.data.data.quote.change}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'separator',
+                  margin: 'xxl'
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  margin: 'xxl',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: 'chartMin',
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘的開盤價',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: `${chartData.open}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘的最高價',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: `${chartData.high}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘的最低價',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: `${chartData.low}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘的收盤價',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: `${chartData.close}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                },
+                {
+                  type: 'box',
+                  layout: 'horizontal',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: '此分鐘的交易張數',
+                      size: 'sm',
+                      color: '#555555'
+                    },
+                    {
+                      type: 'text',
+                      text: `${chartData.unit}`,
+                      size: 'sm',
+                      color: '#111111',
+                      align: 'end'
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              type: 'separator',
+              margin: 'xxl'
+            },
+            {
+              type: 'box',
+              layout: 'horizontal',
+              margin: 'md',
+              contents: [
+                {
+                  type: 'text',
+                  text: '歷史資料查詢',
+                  size: 'xs',
+                  color: '#aaaaaa',
+                  flex: 0,
+                  action: {
+                    type: 'uri',
+                    label: 'action',
+                    uri: `https://www.google.com/finance/quote/${encodeURI(event.message.text)}:TPE?window=MAX`
+                  }
+                },
+                {
+                  type: 'text',
+                  text: `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()} ${new Date().getHours()}:${
+                    new Date().getMinutes() + 1
+                  }`,
+                  color: '#aaaaaa',
+                  size: 'xs',
+                  align: 'end'
+                }
+              ]
+            }
+          ]
+        },
+        styles: {
+          footer: {
+            separator: true
+          }
+        }
       }
 
-      const reply = `股票中文簡稱：${responseMeta.data.data.meta.nameZhTw}
-          \n股票代號：${responseMeta.data.data.info.symbolId}
-          \n今日參考價：${responseMeta.data.data.meta.priceReference}
-          \n漲停價：${responseMeta.data.data.meta.priceHighLimit}
-          \n跌停價：${responseMeta.data.data.meta.priceLowLimit}
-          \n當日股價之漲跌幅：${responseQuote.data.data.quote.change}
+      const message = {
+        type: 'flex',
+        altText: `${event.message.text} Stock Market Live`,
+        contents: {
+          type: 'carousel',
+          contents: [flex]
+        }
+      }
 
-
-          \n此分鐘：${chartMin}
-          \n此分鐘的開盤價：${chartData.open}
-          \n此分鐘的最高價：${chartData.high}
-          \n此分鐘的最低價：${chartData.low}
-          \n此分鐘的收盤價：${chartData.close}
-          \n此分鐘的交易張數：${chartData.unit}
-
-          \n成交時間：${dealtsData.at.substr(11, 8)}
-          \n成交價格：${dealtsData.price}
-          \n成交張數：${dealtsData.unit}
-
-          \n最新一筆成交時間：${new Date(responseQuote.data.data.quote.total.at).toLocaleString('zh-tw')}
-
-          \n歷史資料查詢：${googleFin}
-          `
-      event.reply(reply)
+      fs.writeFileSync('stock-text.json', JSON.stringify(message, null, 2))
+      event.reply(message)
     } catch (error) {
       console.log(error)
       // event.reply(error)
       event.reply('發生錯誤QQ')
     }
-    // } else {
-    //   event.reply('沒有這個股票喔OwO')
-    // }
   }
 })
---
+++ test-line-message.js
@@ -0,0 +1,271 @@
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Detail",
+        "weight": "bold",
+        "color": "#1DB446",
+        "size": "sm"
+      },
+      {
+        "type": "text",
+        "text": "股票代號",
+        "weight": "bold",
+        "size": "xxl",
+        "margin": "md"
+      },
+      {
+        "type": "text",
+        "text": "股票中文簡稱",
+        "size": "xs",
+        "color": "#aaaaaa",
+        "wrap": true
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "xxl",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "今日參考價",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "$2.99",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "漲停價",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "$0.99",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "跌停價",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "$3.33",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "今日漲跌幅",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "$2.99",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "separator",
+            "margin": "xxl"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "margin": "xxl",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "3",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘的開盤價",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$7.31",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘的最高價",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$8.0",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘的最低價",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$0.69",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘的收盤價",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$7.31",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "此分鐘的交易張數",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$7.31",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "margin": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "歷史資料查詢",
+            "size": "xs",
+            "color": "#aaaaaa",
+            "flex": 0
+          },
+          {
+            "type": "text",
+            "text": "now time",
+            "color": "#aaaaaa",
+            "size": "xs",
+            "align": "end"
+          }
+        ]
+      }
+    ]
+  },
+  "styles": {
+    "footer": {
+      "separator": true
+    }
+  }
+}
