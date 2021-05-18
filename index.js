867c9a5bd07b05b31596e9ae351d527d483900c3
showanne <showanne.e@gmail.com>
2021-05-18 23:19:26 +0800

    初步嘗試回復2個 flex 訊息成功，可是有時會失敗

--- index.js
+++ index.js
@@ -46,98 +46,368 @@ bot.listen('/', process.env.PORT, () => {
 bot.on('message', async event => {
   console.log(event)
 
-  if (event.type === 'postback') {
-    console.log(event.postback.data)
-    event.reply(event.postback.data)
-  } else if (event.type === 'message') {
+  if (event.type === 'message') {
     console.log(arrSymbolId.includes(event.message.text))
     try {
-      if (arrSymbolId.includes(event.message.text)) {
-        const flex = [
-          {
-            type: 'bubble',
-            hero: {
-              type: 'image',
-              url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-              size: 'full',
-              aspectMode: 'cover'
-            },
-            body: {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: `${event.message.text}`,
-                  weight: 'bold',
-                  size: 'xl',
-                  align: 'center'
+      if (event.message.type === 'text') {
+        if (event.message.text.includes('news')) {
+          const responseSearch = await axios.get(`https://api.cnyes.com/media/api/v1/search?q=${encodeURI(event.message.text.substr(0, 4))}`)
+
+          const newsArr = responseSearch.data.items.data
+
+          const flex = [
+            {
+              type: 'bubble',
+              size: 'micro',
+              hero: {
+                type: 'image',
+                url: `${newsArr[0].coverSrc.xl.src}`,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '320:213',
+                action: {
+                  type: 'uri',
+                  label: 'action',
+                  uri: 'http://linecorp.com/'
                 }
-              ]
+              },
+              body: {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `${newsArr[0].title}`,
+                    weight: 'bold',
+                    size: 'sm',
+                    wrap: true,
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: `https://news.cnyes.com/news/id/${encodeURI(newsArr[0].newsId)}`
+                    },
+                    maxLines: 3
+                  },
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    contents: [
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png'
+                      },
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png'
+                      },
+                      {
+                        type: 'text',
+                        text: `${new Date(newsArr[0].publishAt * 1000).toLocaleString('zh-tw').substr(0, 10)}`,
+                        size: 'xs',
+                        color: '#8c8c8c',
+                        margin: 'md',
+                        flex: 0
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        spacing: 'sm',
+                        contents: [
+                          {
+                            type: 'text',
+                            text: `${newsArr[0].keyword.join('、')}`,
+                            wrap: false,
+                            style: 'italic',
+                            color: '#8c8c8c',
+                            size: 'xs',
+                            flex: 5
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+                spacing: 'sm',
+                paddingAll: '13px'
+              }
             },
-            footer: {
-              type: 'box',
-              layout: 'vertical',
-              spacing: 'sm',
-              contents: [
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'postback',
-                    label: 'stock live',
-                    data: `${event.message.text}`
-                  }
-                },
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'message',
-                    label: 'stock news',
-                    text: `${event.message.text}`
-                  }
-                },
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'uri',
-                    label: 'stock history',
-                    uri: `https://www.google.com/finance/quote/${encodeURI(event.message.text)}:TPE?window=MAX`
-                  }
-                },
-                {
-                  type: 'spacer',
-                  size: 'sm'
+            {
+              type: 'bubble',
+              size: 'micro',
+              hero: {
+                type: 'image',
+                url: `${newsArr[1].coverSrc.xl.src}`,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '320:213',
+                action: {
+                  type: 'uri',
+                  label: 'action',
+                  uri: 'http://linecorp.com/'
                 }
-              ],
-              flex: 0
+              },
+              body: {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `${newsArr[1].title}`,
+                    weight: 'bold',
+                    size: 'sm',
+                    wrap: true,
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: `https://news.cnyes.com/news/id/${encodeURI(newsArr[1].newsId)}`
+                    },
+                    maxLines: 3
+                  },
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    contents: [
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png'
+                      },
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png'
+                      },
+                      {
+                        type: 'text',
+                        text: `${new Date(newsArr[1].publishAt * 1000).toLocaleString('zh-tw').substr(0, 10)}`,
+                        size: 'xs',
+                        color: '#8c8c8c',
+                        margin: 'md',
+                        flex: 0
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        spacing: 'sm',
+                        contents: [
+                          {
+                            type: 'text',
+                            text: `${newsArr[1].keyword.join('、')}`,
+                            wrap: false,
+                            style: 'italic',
+                            color: '#8c8c8c',
+                            size: 'xs',
+                            flex: 5
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+                spacing: 'sm',
+                paddingAll: '13px'
+              }
+            },
+            {
+              type: 'bubble',
+              size: 'micro',
+              hero: {
+                type: 'image',
+                url: `${newsArr[2].coverSrc.xl.src}`,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '320:213',
+                action: {
+                  type: 'uri',
+                  label: 'action',
+                  uri: 'http://linecorp.com/'
+                }
+              },
+              body: {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `${newsArr[2].title}`,
+                    weight: 'bold',
+                    size: 'sm',
+                    wrap: true,
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: `https://news.cnyes.com/news/id/${encodeURI(newsArr[2].newsId)}`
+                    },
+                    maxLines: 3
+                  },
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    contents: [
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png'
+                      },
+                      {
+                        type: 'icon',
+                        size: 'xs',
+                        url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png'
+                      },
+                      {
+                        type: 'text',
+                        text: `${new Date(newsArr[2].publishAt * 1000).toLocaleString('zh-tw').substr(0, 10)}`,
+                        size: 'xs',
+                        color: '#8c8c8c',
+                        margin: 'md',
+                        flex: 0
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        spacing: 'sm',
+                        contents: [
+                          {
+                            type: 'text',
+                            text: `${newsArr[2].keyword.join('、')}`,
+                            wrap: false,
+                            style: 'italic',
+                            color: '#8c8c8c',
+                            size: 'xs',
+                            flex: 5
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+                spacing: 'sm',
+                paddingAll: '13px'
+              }
+            }
+          ]
+          const message = {
+            type: 'flex',
+            altText: `${event.message.text} Stock News`,
+            contents: {
+              type: 'carousel',
+              contents: flex
             }
           }
-        ]
 
-        const message = {
-          type: 'flex',
-          altText: `${event.message.text} Stock News`,
-          contents: {
-            type: 'carousel',
-            contents: flex
-          }
+          fs.writeFileSync('stock-news.json', JSON.stringify(message, null, 2))
+          event.reply(message)
         }
 
-        fs.writeFileSync('stock-menu.json', JSON.stringify(message, null, 2))
-        event.reply(message)
-      } else {
-        event.reply('查無此股票')
+        // if (event.message.text.includes('live')) {}
+
+        if (arrSymbolId.includes(event.message.text)) {
+          const flex = [
+            {
+              type: 'bubble',
+              hero: {
+                type: 'image',
+                url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                size: 'full',
+                aspectMode: 'cover'
+              },
+              body: {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `${event.message.text}`,
+                    weight: 'bold',
+                    size: 'xl',
+                    align: 'center'
+                  }
+                ]
+              },
+              footer: {
+                type: 'box',
+                layout: 'vertical',
+                spacing: 'sm',
+                contents: [
+                  {
+                    type: 'button',
+                    style: 'link',
+                    height: 'sm',
+                    action: {
+                      type: 'postback',
+                      label: 'stock live',
+                      data: `${event.message.text}`
+                    }
+                  },
+                  {
+                    type: 'button',
+                    style: 'link',
+                    height: 'sm',
+                    action: {
+                      type: 'message',
+                      label: 'stock news',
+                      text: `${event.message.text} news`
+                    }
+                  },
+                  {
+                    type: 'button',
+                    style: 'link',
+                    height: 'sm',
+                    action: {
+                      type: 'uri',
+                      label: 'stock history',
+                      uri: `https://www.google.com/finance/quote/${encodeURI(event.message.text)}:TPE?window=MAX`
+                    }
+                  },
+                  {
+                    type: 'spacer',
+                    size: 'sm'
+                  }
+                ],
+                flex: 0
+              }
+            }
+          ]
+
+          const message = {
+            type: 'flex',
+            altText: `${event.message.text} Stock Menu`,
+            contents: {
+              type: 'carousel',
+              contents: flex
+            }
+          }
+
+          fs.writeFileSync('stock-menu.json', JSON.stringify(message, null, 2))
+          event.reply(message)
+        } else {
+          event.reply('查無此股票')
+        }
       }
     } catch (error) {
       console.log(error)
       // event.reply(error)
-      event.reply('發生錯誤QQ')
+      event.reply('發生錯誤 QQ')
     }
   }
 })
