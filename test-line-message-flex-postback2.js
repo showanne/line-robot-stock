3dc0de3a170ffbda69e8be0d87f725b2ae0a1e0a
showanne <showanne.e@gmail.com>
2021-05-19 23:22:13 +0800

    更改回復樣式

---
+++ test-line-message-flex-postback2.js
@@ -0,0 +1,198 @@
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "image",
+        "url": "https://www.frevvo.com/blog/wp-content/uploads/2020/01/Frevvo-Improve-Finance-hero.png",
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "15:19",
+        "gravity": "top"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "filler"
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "icon",
+                    "url": "https://img.icons8.com/nolan/344/stock-share.png"
+                  },
+                  {
+                    "type": "text",
+                    "text": "stock market live",
+                    "color": "#2A1E5C",
+                    "flex": 0,
+                    "offsetTop": "-2px"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "spacing": "sm"
+              },
+              {
+                "type": "filler"
+              }
+            ],
+            "borderWidth": "1px",
+            "cornerRadius": "4px",
+            "spacing": "sm",
+            "borderColor": "#8B80F9",
+            "margin": "xxl",
+            "height": "40px",
+            "action": {
+              "type": "postback",
+              "label": "action",
+              "data": "stock market live"
+            }
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "filler"
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "icon",
+                    "url": "https://img.icons8.com/nolan/344/card-exchange.png"
+                  },
+                  {
+                    "type": "text",
+                    "text": "stock news",
+                    "color": "#2A1E5C",
+                    "flex": 0,
+                    "offsetTop": "-2px"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "spacing": "sm"
+              },
+              {
+                "type": "filler"
+              }
+            ],
+            "borderWidth": "1px",
+            "cornerRadius": "4px",
+            "spacing": "sm",
+            "borderColor": "#8B80F9",
+            "margin": "xxl",
+            "height": "40px",
+            "action": {
+              "type": "postback",
+              "label": "action",
+              "data": "stock news"
+            }
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "filler"
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "icon",
+                    "url": "https://img.icons8.com/nolan/344/pay-date.png"
+                  },
+                  {
+                    "type": "text",
+                    "text": "stock history",
+                    "color": "#2A1E5C",
+                    "flex": 0,
+                    "offsetTop": "-2px"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "spacing": "sm",
+                "action": {
+                  "type": "uri",
+                  "label": "action",
+                  "uri": "http://linecorp.com/"
+                }
+              },
+              {
+                "type": "filler"
+              }
+            ],
+            "cornerRadius": "4px",
+            "spacing": "sm",
+            "borderColor": "#8B80F9",
+            "margin": "xxl",
+            "height": "40px",
+            "borderWidth": "1px"
+          }
+        ],
+        "position": "absolute",
+        "offsetBottom": "0px",
+        "offsetStart": "0px",
+        "offsetEnd": "0px",
+        "backgroundColor": "#6CD4FF66",
+        "paddingAll": "36px",
+        "paddingTop": "19px"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "0000",
+            "color": "#EAD94C",
+            "align": "center",
+            "size": "xxl",
+            "offsetTop": "3px",
+            "margin": "xs",
+            "gravity": "center",
+            "wrap": false,
+            "adjustMode": "shrink-to-fit",
+            "style": "italic"
+          }
+        ],
+        "position": "absolute",
+        "cornerRadius": "15px",
+        "offsetTop": "18px",
+        "backgroundColor": "#6CD4FFdd",
+        "offsetStart": "18px",
+        "height": "49px",
+        "width": "99px"
+      }
+    ],
+    "paddingAll": "0px"
+  }
+}
