6a43286023115fe2d7362d4ae6a33551c542415e
showanne <showanne.e@gmail.com>
2021-05-28 23:41:27 +0800

    說明的樣式 待調整

---
+++ test-line-message-flex-intro.js
@@ -0,0 +1,177 @@
+{
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://www.frevvo.com/blog/wp-content/uploads/2020/01/Frevvo-Improve-Finance-hero.png",
+            "size": "full",
+            "aspectMode": "cover",
+            "aspectRatio": "150:196",
+            "gravity": "center",
+            "flex": 1
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Stock",
+                "size": "xs",
+                "color": "#ffffff",
+                "align": "center",
+                "gravity": "center"
+              }
+            ],
+            "backgroundColor": "#EC3D44",
+            "paddingAll": "2px",
+            "paddingStart": "4px",
+            "paddingEnd": "4px",
+            "flex": 0,
+            "position": "absolute",
+            "offsetStart": "18px",
+            "offsetTop": "18px",
+            "cornerRadius": "100px",
+            "width": "48px",
+            "height": "25px"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "contents": [],
+                    "size": "xl",
+                    "wrap": true,
+                    "text": "歡迎來到…",
+                    "color": "#ffffff",
+                    "weight": "bold"
+                  },
+                  {
+                    "type": "text",
+                    "text": "在這個聊天室您可以直接輸入股票代號，協助您快速查詢股票資訊！",
+                    "color": "#ffffffcc",
+                    "size": "sm",
+                    "wrap": true
+                  }
+                ],
+                "spacing": "sm"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "contents": [],
+                        "size": "sm",
+                        "wrap": true,
+                        "margin": "lg",
+                        "color": "#ffffffde",
+                        "text": "或輸入\n 股票代號 news\n  股票代號 market\n  股票代號 history\n  立刻回覆你商品資訊",
+                        "align": "start"
+                      }
+                    ],
+                    "paddingAll": "19px"
+                  }
+                ],
+                "paddingAll": "13px",
+                "backgroundColor": "#ffffff1A",
+                "cornerRadius": "2px",
+                "margin": "xl"
+              }
+            ],
+            "spacing": "sm",
+            "position": "absolute",
+            "offsetTop": "50px",
+            "offsetStart": "50px",
+            "backgroundColor": "#064d88dd",
+            "paddingAll": "30px",
+            "offsetBottom": "none"
+          }
+        ]
+      }
+    ],
+    "paddingAll": "0px"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "contents": [],
+                "size": "xl",
+                "wrap": true,
+                "text": "歡迎來到…",
+                "color": "#ffffff",
+                "weight": "bold"
+              },
+              {
+                "type": "text",
+                "text": "在這個聊天室您可以直接輸入股票代號，協助您快速查詢股票資訊！",
+                "color": "#ffffffcc",
+                "size": "sm",
+                "wrap": true
+              }
+            ],
+            "spacing": "sm"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "contents": [],
+                    "size": "sm",
+                    "wrap": true,
+                    "margin": "lg",
+                    "color": "#ffffffde",
+                    "text": "或輸入\n 股票代號 news\n  股票代號 market\n  股票代號 history\n  立刻回覆你商品資訊",
+                    "align": "start"
+                  }
+                ],
+                "paddingStart": "30px"
+              }
+            ],
+            "paddingAll": "13px",
+            "backgroundColor": "#ffffff1A",
+            "cornerRadius": "2px",
+            "margin": "xl"
+          }
+        ]
+      }
+    ],
+    "paddingAll": "20px",
+    "backgroundColor": "#064d88"
+  }
+}
